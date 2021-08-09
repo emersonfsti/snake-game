378a31c3d7628a1d442a3247d6a1ca48db1da866
emersonfsti <emersonfsti@gmail.com>
2021-08-09 18:47:59 -0300

    create snakë

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
+// snake como array
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
-// Definir Campo do jogo
+// função Definir Campo do jogo
 function criarBG() {
     // cor da tela
     context.fillStyle =  "lightgreen";
@@ -10,5 +16,14 @@ function criarBG() {
     // tamanho da tela
     context.fillRect(0,0, 16 * box, 16 * box);
 }
+// função criar snake (cobra)
+function snakeLitle() {
+    for(i=0; i < snake.length; i++){
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 //iniciar function
 criarBG();
+snakeLitle();
